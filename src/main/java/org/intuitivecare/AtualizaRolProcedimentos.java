dea475f491292689915622477042bc2b5245c52a
Pedro Basilio <phbasilio6903@gmail.com>
2025-03-25 20:42:08 -0300

    Feat: Adicionei o metodo que realiza a configuração do RPA, e o acesso ao site

--- src/main/java/org/intuitivecare/AtualizaRolProcedimentos.java
+++ src/main/java/org/intuitivecare/AtualizaRolProcedimentos.java
@@ -1,14 +1,33 @@
 package org.intuitivecare;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AtualizaRolProcedimentos {
 
-    public void ConfiguraRobo(){
-        String chromeDriverPath = System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe";
-        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-        WebDriver driver = new ChromeDriver();
+    private WebDriver driver;
+    public boolean configuraRobo() {
+        boolean config = false;
+        try {
+            String chromeDriverPath = System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe";
+            System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+            driver = new ChromeDriver();
+
+            config = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return config;
+    }
+
+    public  void EntraNoSite(){
+        driver.get("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos");
+
+    }
+
+
+    public void AceitaCookies(){
 
     }
 }
--- src/main/java/org/intuitivecare/Main.java
+++ src/main/java/org/intuitivecare/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         AtualizaRolProcedimentos robo = new AtualizaRolProcedimentos();
 
-        robo.ConfiguraRobo();
+        if(robo.configuraRobo()){
+            robo.EntraNoSite();
+        }
     }
 }
