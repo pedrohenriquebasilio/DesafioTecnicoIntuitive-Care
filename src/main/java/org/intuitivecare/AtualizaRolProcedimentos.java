4e8e4755cd6f2720009602f8ea2851807d90568b
Pedro Basilio <phbasilio6903@gmail.com>
2025-03-25 21:06:53 -0300

    Feat: Adicionei o metodo que será auxiliar para inibir erros, será utilizado para verificações de seletores, e consequentemente uso para ramificações no codigo.

--- src/main/java/org/intuitivecare/AtualizaRolProcedimentos.java
+++ src/main/java/org/intuitivecare/AtualizaRolProcedimentos.java
@@ -1,8 +1,14 @@
 package org.intuitivecare;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class AtualizaRolProcedimentos {
 
@@ -23,11 +29,21 @@ public class AtualizaRolProcedimentos {
 
     public  void EntraNoSite(){
         driver.get("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos");
-
+        if(foiEncontradoElemento(driver,By.xpath("/html/body/div[5]/div/div/div/div/div[2]/button[3]"))){
+            System.out.println("achei");
+        } else {
+            System.out.println("não achei");
+        }
     }
 
 
-    public void AceitaCookies(){
-
+    public static boolean foiEncontradoElemento(WebDriver driver, By by) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.presenceOfElementLocated(by));
+            return true;
+        } catch (TimeoutException e) {
+            return false;
+        }
     }
 }
